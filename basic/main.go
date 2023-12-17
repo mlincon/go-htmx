f5a78ec4f7cb6afd4c5b005c90e0069e44f41852
M Lincon <49597604+mlincon@users.noreply.github.com>
2023-12-17 20:45:35 +0000

    completion of basic web server with go

--- basic/main.go
+++ basic/main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
 type Film struct {
@@ -29,7 +30,46 @@ func main() {
 		tmpl.Execute(w, films)
 	}
 
+	h2 := func(w http.ResponseWriter, r *http.Request) {
+		log.Print("HTMX request received")
+		log.Print(r.Header.Get("HX-Request")) // if true it indicates that the request is comming from htmx
+
+		// simulate some latency/external dependency like database to show the loading icon
+		// on submit click
+		time.Sleep(1 * time.Second)
+
+		title := r.PostFormValue("title")       // get the data from the index.html input with the name "title"
+		director := r.PostFormValue("director") // get the data from the index.html input with the name "director"
+
+		/* This shows one option to render the html template, where we create a simple
+		string and pass it onto the template
+
+		htmlStr := fmt.Sprintf(
+			"<li class='list-group-item bg-primary text-width'>"+
+				"%s - %s"+
+				"</li>", title, director,
+		)
+
+		// now create a new Template object. The name of the object does not matter
+		// here it is called "t"
+		// parse the formatted string to the parser
+		tmpl, _ := template.New("t").Parse(htmlStr)
+		tmpl.Execute(w, nil)
+		*/
+
+		/* Alternatively use the ExecuteTemplate to target the block in the html
+		as shown below. This way we do not have to hard-code any html within the code
+		*/
+
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.ExecuteTemplate(w, "film-list-element", Film{
+			Title:    title,
+			Director: director,
+		})
+	}
+
 	http.HandleFunc("/", h1)
+	http.HandleFunc("/add-film/", h2)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
--- basic/readme.md
+++ basic/readme.md
@@ -1,1 +1,8 @@
-Based on: https://www.youtube.com/watch?v=F9H6vYelYyU
+## Go Server and HTML template Rending using standard libraries and htmx
+
+Based on:
+
+- https://www.youtube.com/watch?v=F9H6vYelYyU
+- https://github.com/bugbytes-io/htmx-go-demo/tree/master
+
+![Alt text](image.png)
