031194e30d513b156c9566558f4937bb0d9649c2
M Lincon <49597604+mlincon@users.noreply.github.com>
2023-12-18 13:40:25 +0000

    connect to database

--- todo-app/model/db.go
+++ todo-app/model/db.go
@@ -2,7 +2,9 @@ package model
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
@@ -14,5 +16,35 @@ func Setup() {
 	dotenv := ".local.env"
 	if err := godotenv.Load(dotenv); err != nil {
 		log.Fatal("Error loading .env file")
+	}
+
+	// Retrieve values from environment variables
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("DB_SSLMODE")
+
+	// Construct the connection string
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		host,
+		port,
+		user,
+		password,
+		dbname,
+		sslmode,
+	)
+
+	var err error
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		fmt.Println("Could not connect to db", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Could not ping db", err)
 	}
 }
