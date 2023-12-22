0147cf443352d86b4d3af5f3ae9c4e4a3c1e9402
M Lincon <49597604+mlincon@users.noreply.github.com>
2023-12-22 13:53:57 +0000

    add initial sql script

---
+++ form-input/initdb/init.sql
@@ -0,0 +1,24 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; --noqa: RF05
+
+CREATE SCHEMA IF NOT EXISTS form;
+
+DROP TABLE IF EXISTS form.users;
+CREATE TABLE form.users (
+    user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(), --noqa: CP03
+    name TEXT,
+    created_ts TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS form.comments;
+CREATE TABLE form.comments (
+    comment_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id CHAR(36), -- uuid
+    product TEXT,
+    category TEXT,
+    comment TEXT,
+    created_ts TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+ALTER TABLE form.comments
+ADD CONSTRAINT fk_comment_user FOREIGN KEY (
+    user_id
+) REFERENCES form.users (user_id);
